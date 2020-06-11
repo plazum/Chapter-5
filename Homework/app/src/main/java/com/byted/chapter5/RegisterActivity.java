e68beb5080113bf5eabf0fecf0000f1e32283fd0
plazum <34277374+plazum@users.noreply.github.com>
2020-06-11 18:49:54 +0800

    Homework

--- Homework/app/src/main/java/com/byted/chapter5/RegisterActivity.java
+++ Homework/app/src/main/java/com/byted/chapter5/RegisterActivity.java
@@ -47,8 +47,21 @@ public class RegisterActivity extends AppCompatActivity {
                     return;
                 }
                 // todo 做网络请求
+                apiService.register(name, password, repassword).enqueue(new Callback<UserResponse>() {
+                    @Override
+                    public void onResponse(Call<UserResponse> call, Response<UserResponse> response) {
+                        if (response.body() != null)
+                            if (response.body().user == null)
+                                Toast.makeText(RegisterActivity.this, "注册失败：" + response.body().errorMsg, Toast.LENGTH_SHORT).show();
+                            else
+                                Toast.makeText(RegisterActivity.this, "注册成功：" + response.body().user.nickname, Toast.LENGTH_SHORT).show();
+                    }
 
-
+                    @Override
+                    public void onFailure(Call<UserResponse> call, Throwable t) {
+                        Toast.makeText(RegisterActivity.this, "网络失败", Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
     }
